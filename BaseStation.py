3783eb13c9b3292cf721cd54b49f79dfbd45dfef
Hichem Ghandri <hichem.ghandri@telecom-paristeh.fr>
2018-05-27 20:29:41 +0800

    dernier commit BS

--- BaseStation.py
+++ BaseStation.py
@@ -3,11 +3,11 @@
 Script class base station
  
 """
-from threading import  RLock
+import threading 
 import thread
 import time
 import socket
-import MsgHandler
+from  MsgHandler import MsgHandler 
 import copy
 
 
@@ -22,7 +22,7 @@ class BaseStation():
         self._coordinate   = [0,0]
         self.network = []
         self.allSensorData = "xixixixixi"
-        self.networkLock = RLock()
+        self.networkLock = threading.Lock()
         self.clusterH = []
         self.adrrCH = "0.0.0.0"
         
@@ -54,6 +54,7 @@ class BaseStation():
     
     
     def receive(self,addr_source, port_source):
+        print 'thread start'
         code=0
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.bind((addr_source, port_source)) 
@@ -79,8 +80,10 @@ class BaseStation():
                     print "error in decoding list of info msg"
             elif type_msg==3:
                 temp,code=msgHandler.Decode_Info_Msg(data)
-                if code==0:                 
+                if code==0:
+                    print '-----------------3.--------'
                     Ischanged=self.RefreshNode(temp)
+                    
                     print temp
                     print Ischanged
                 else:
@@ -91,8 +94,8 @@ class BaseStation():
                     self.allSensorData=self.allSensorData+temp  
                     with self.networkLock:
                         for i in range(len(self.network)):
-                            if self.network[i][0] == self.addrCH:
-                                self.network[i][3] = time.time()
+                            if self.network[i][0][0] == self.addrCH:
+                                self.network[i][0][3] = time.time()
                     print temp
                 else:
                     print 'Error in decoding sensor data'               
@@ -106,24 +109,26 @@ class BaseStation():
         coor    = [0,0]
         IsChange= False
         address      =  info[0]
-        energy_level =  info[1]
+        energy_level =  int(info[1])
         coor         =  info[2]
         try:
-            with self.networkLock:
-                for i in range(len(self.network)):
-                    if self.network[i][0] == address:
-                        self.network[i][1] = energy_level
-                        self.network[i][2] = coor
-                        self.network[i][3] = time.time()
-                        IsChange = True
-                        break
-                if IsChange==False:
-                    self.network.append(info)
-                    self.network[len(self.network)][3] = time.time()
-                
+            print'rentre ds le lock' 
+            print len(self.network)
+            for i in range(len(self.network)):
+                if self.network[i][0][0] == address:
+                    self.network[i][0][1] = int(energy_level)
+                    self.network[i][0][2] = coor
+                    self.network[i][0][3] = time.time()
+                    IsChange = True
+                    break
+            if IsChange==False:
+                print str(info)
+                self.network.append([info,time.time()])
+                #self.network[len(self.network)][3] = time.time()    
         except :
             print 'info updata failed.'
             code=1
+        return IsChange
     
     
     def send(self, addr_des, port_des, msg):
@@ -135,14 +140,17 @@ class BaseStation():
             print 'sent:' + msg
         except:
             code=1
+            print 'send error.'
         finally:
             s.close()
             
     
     def alive(self, addr):
+        print(len(self.network))
         for i in range(len(self.network)):
-            if self.network[i][0] == addr:
-                if (time.time()-self.network[i][3])<60:
+            if self.network[i][0][0] == addr:
+                print(i)
+                if (time.time()-self.network[i][1])<1000:
                     return 1
                 else:
                     return 0 
@@ -150,30 +158,34 @@ class BaseStation():
                 
     def toBeCH(self):
         listNodeNoCH=[]
-        with self.networkLock:
-            
-            for i in range(len(self.network)):
-                NodeCH = False
-                for j in range(len(self.clusterH)):
-                    if self.network[i][0] == self.clusterH[j]:
-                        NodeCH = True 
-                if NodeCH == False and self.alive(self.network[i][0]) == 1:
-                    listNodeNoCH.append(self.network[i])
-                    
-            if len(listNodeNoCH) == 0:
-                listNodeNoCH = copy.deepcopy(self.network)
-                self.clusterH [:] = []
+        for i in range(len(self.network)):
+            NodeCH = False
+            for j in range(len(self.clusterH)):
+                if self.network[i][0][0] == self.clusterH[j][0]:
+                    NodeCH = True 
+            if NodeCH == False and self.alive(self.network[i][0][0]) == 1:
+                listNodeNoCH.append(self.network[i])
+        
+        if len(listNodeNoCH) == 0:  
+            listNodeNoCH = copy.deepcopy(self.network)
+            self.clusterH [:] = []
+        print('lisnoch')
+        print(listNodeNoCH )
 
-        e = listNodeNoCH[0][1]
-        for i in range(1,listNodeNoCH):
-            if listNodeNoCH[i][1] > e:
-                self.adrrCH = listNodeNoCH[i][0]
-                e           = listNodeNoCH[i][1]
-                coorCH      = listNodeNoCH[i][2]
-        self.clusterH.append(self.adrrCH)
+        e = listNodeNoCH[0][0][1]
+        print("ener",e)
+        coorCH = listNodeNoCH[0][0][2]
+        for i in range(len(listNodeNoCH)):
+            if listNodeNoCH[i][0][1] >= e:
+                self.adrrCH = listNodeNoCH[i][0][0]
+                e           = listNodeNoCH[i][0][1]
+                coorCH      = listNodeNoCH[i][0][2]
+        self.clusterH.append([self.adrrCH,e])
+        print(self.clusterH)
         msgHandler = MsgHandler()
+        print(coorCH)
         msgCH = msgHandler.Encode_CH_Change_Msg(self.adrrCH,e,coorCH)
-        print("New Cluster Head :", self.adrrCH)
+        print("New Cluster Head :", self.adrrCH, e)
         
         return self.adrrCH,msgCH
             
@@ -194,23 +206,63 @@ class BaseStation():
 if __name__ == '__main__':
     base    = BaseStation()
     choseCH = time.time()
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    try :
+        thread.start_new_thread(base.receive,( '', 14800,))
+    except:
+        print 'no'
+        
+    time.sleep(1)
+    msgHandler=MsgHandler()
+    msg = msgHandler.Encode_Info_Msg('12',100,[3,5])
+    try:
+        s.sendto(msg, ('127.0.0.1',14800))
+        print 'sent:' + msg
+    except:
+        print'no send'
+    '''finally:
+        s.close()'''
+
+    msg = msgHandler.Encode_Info_Msg('1',110,[2,8])
+    try:
+        s.sendto(msg, ('127.0.0.1',14800))
+        print 'sent:' + msg
+    except:
+        print'no send'
+    '''finally:
+        s.close()'''
+        
+    msg = msgHandler.Encode_Info_Msg('2',80,[2,4])
+    try:
+        s.sendto(msg, ('127.0.0.1',14800))
+        print 'sent:' + msg
+    except:
+        print'no send'
+    finally:
+        s.close()
+
+        
+    
     while(1):
-        thread.start_new_thread(base.receive,( base._get_IPadress, 12800))
+        time.sleep(1)
         if len(base._get_network()) == 2:
            adrrCH, msgCH = base.toBeCH()
-           base.send(adrrCH,12800,msgCH)
+           base.send('127.0.0.1',12800,msgCH)
            choseCH=time.time()
+           print(adrrCH+'1')
         
-        if(time.time()-choseCH)>30:
+        if(time.time()-choseCH)>5:
             adrrCH, msgCH = base.toBeCH()
-            base.send(adrrCH,12800,msgCH)
+            base.send('127.0.0.1',12800,msgCH)
             choseCH=time.time()
+            print(adrrCH+'2')
             
         if base.alive(base._get_adrrCH()) == 0:
             adrrCH, msgCH = base.toBeCH()
-            base.send(adrrCH,12800,msgCH)
+            base.send('127.0.0.1',12800,msgCH)
             choseCH=time.time()
-        
+            print(adrrCH+'3')
        
         
         
